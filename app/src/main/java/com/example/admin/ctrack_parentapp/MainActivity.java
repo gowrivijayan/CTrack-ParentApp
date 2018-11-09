10b53428bb1fb41dcb0b8dc2677cadd8c24298ab
veenadraveendran <veenadraveendran@gmail.com>
2018-11-09 14:06:12 +0530

    register java

--- app/src/main/java/com/example/admin/ctrack_parentapp/MainActivity.java
+++ app/src/main/java/com/example/admin/ctrack_parentapp/MainActivity.java
@@ -1,9 +1,30 @@
 package com.example.admin.ctrack_parentapp;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText regemail,regpass,regconpass;
+    private String email,pass,conpass;
+    private FirebaseAuth mAuth;
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences.Editor editor;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
