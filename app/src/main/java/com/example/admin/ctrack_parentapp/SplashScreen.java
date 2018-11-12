53803fc589d34dccba253a9774759f1191ce3dc0
Aiswarya <aiswaryasr902@gmail.com>
2018-11-12 12:04:01 +0530

    splashscreen java1

--- app/src/main/java/com/example/admin/ctrack_parentapp/SplashScreen.java
+++ app/src/main/java/com/example/admin/ctrack_parentapp/SplashScreen.java
@@ -1,9 +1,23 @@
-package com.example.admin.ctrack_parentapp;
+package com.example.devoprakesh.trackmychild;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
+import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class SplashScreen extends AppCompatActivity {
+
+    private static final int PERMISSION_REQUEST=1;
+    private SharedPreferences sharedPreferences;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
