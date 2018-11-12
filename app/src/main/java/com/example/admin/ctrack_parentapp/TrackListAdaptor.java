fba059ba32e54e3f70b0e6b95dd176806ba6b2e6
Aiswarya <aiswaryasr902@gmail.com>
2018-11-12 19:29:02 +0530

    tracklist java1

--- app/src/main/java/com/example/admin/ctrack_parentapp/TrackListAdaptor.java
+++ app/src/main/java/com/example/admin/ctrack_parentapp/TrackListAdaptor.java
@@ -1,7 +1,16 @@
-package com.example.admin.ctrack_parentapp;
+package com.example.devoprakesh.trackmychild;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class TrackListAdaptor extends AppCompatActivity {
 
